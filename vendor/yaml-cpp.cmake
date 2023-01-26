b7be8dc093aaba1b885261da6ecc7e7d60e43e97
David Allemang <david.allemang@kitware.com>
2023-01-26 14:22:20 -0500

    COMP: Use fetchcontent for vendor dependencies.
    Remove git submodules entirely.

---
+++ vendor/yaml-cpp.cmake
@@ -0,0 +1,11 @@
+FetchContent_Declare(
+    yaml-cpp
+    GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
+    GIT_TAG yaml-cpp-0.7.0
+    GIT_PROGRESS TRUE
+)
+set(GLFW_BUILD_EXAMPLES OFF CACHE INTERNAL "")
+set(GLFW_BUILD_TESTS OFF CACHE INTERNAL "")
+set(GLFW_BUILD_DOCS OFF CACHE INTERNAL "")
+set(GLFW_INSTALL OFF CACHE INTERNAL "")
+FetchContent_MakeAvailable(yaml-cpp)
